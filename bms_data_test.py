38404574f95e2331e8ee9575ca7d5dd8621fbc0e
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-05-28 21:07:36 +0530

    Create bms_data_test.py

---
+++ bms_data_test.py
@@ -0,0 +1,14 @@
+import unittest
+import bms_sender_data
+
+class test_battery_current_ranges(unittest.TestCase):
+    
+    def test_alertType(self):
+        self.assertTrue(bms_sender_data.battery_Limit_Check(["charge_rate" ,"temperature", "state_of_charge"],"Console"))
+ 
+    def test_bms_limits(self):
+        self.assertTrue(bms_sender_data.battery_is_ok({'temperature': 15,'state_of_charge': 65, 'charge_rate': 0.6}) is True) 
+        self.assertTrue(bms_sender_data.battery_is_ok({'temperature': 70,'state_of_charge': 90, 'charge_rate': 0.9}) is False)  
+      
+ if __name__ == '__main__':
+  unittest.main()
